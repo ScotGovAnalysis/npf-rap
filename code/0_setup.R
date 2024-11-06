338a9a6205e00d353e74691f0330726f04893ed2
calligovscot <calli.dougall@gov.scot>
2024-11-06 11:44:45 +0000

    Creating set up script and folder for code

---
+++ code/0_setup.R
@@ -0,0 +1,28 @@
+##########################################################################
+# Name of file - 0_setup.R
+# Type - Reproducible Analytical Pipeline (RAP)
+# Written/run on - RStudio Desktop
+# Version of R - 4.2.2
+#
+# Description - Sets up environment required for running NPF RAP. 
+# No manual changes are required to this script before the process is run.
+##########################################################################
+
+
+### 1. Load packages ----
+
+library(tidyverse)
+library(janitor)
+
+
+### 2. Create folders ----
+
+
+
+
+### 3. Create functions ----
+
+
+
+
+### END OF SCRIPT ###
